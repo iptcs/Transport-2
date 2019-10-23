6ca9eefaebee66a68c8ca6157200f544687eedee
Isaac <ipt17@hampshire.edu>
2019-10-23 13:40:47 -0700

    Initial commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameController.airplaneSpawned == false)
+        {
+            GameController.airplane = gameObject;
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnMouseDown()
+    {
+        if (GameController.airplane == gameObject && GameController.activePlane != gameObject)
+        {
+            GameController.activePlane = gameObject;
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (GameController.airplane == gameObject && GameController.activePlane == gameObject)
+        {
+            GameController.activePlane = null;
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (GameController.airplane != gameObject && GameController.activePlane != null)
+        {
+            GameController.activePlane.GetComponent<Renderer>().material.color = Color.white;
+            GameController.airplane = gameObject;
+            GameController.activePlane = gameObject;
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    Vector3 cubePosition;
+    public static GameObject airplane;
+    public static GameObject activePlane;
+    public int gridLength = 16, gridHeight = 9;
+    public static bool airplaneSpawned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int i = 0; i < 16; i++)
+        //{
+        //cubePosition = new Vector3(i*2, 0, 0);
+        //Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+        //}
+        for (int y = 0; y < gridHeight; y += 2)
+        {
+            for (int x = 0; x < gridLength; x += 2)
+            {
+                SpawnCube(x, y);
+                if (airplaneSpawned == false)
+                    { airplaneSpawned = true; }
+            }
+        }
+    }
+    void SpawnCube(int Xaxis, int Yaxis)
+    {
+        cubePosition = new Vector3(Xaxis, Yaxis, 0);
+        Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
